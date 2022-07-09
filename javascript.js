812d00e58bc70b263677ba2cba94f81b216360ba
Nguyễn Hòa Hợp <88434939+hopcodedao@users.noreply.github.com>
2022-07-09 11:25:09 +0700

    upload source code

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function redirect1(){
+    window.location.href = "https://lienquan.garena.vn/";
+}
+function redirect2(){
+    window.location.href = "https://www.marvel.com/";
+}
+function redirect3(){
+    window.location.href = "https://mephimle.com/tu-khoa/harry-potter";
+}
+function redirect4(){
+    window.location.href = "https://www.ssphim.net/phim/mo-dom-dom-grave-of-the-fireflies";
+}
+function redirect5(){
+    window.location.href = "https://www.ssphim.net/phim/avatar-the-than-2009";
+}
